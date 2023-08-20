d586eda0c08dba3b6406ac666a71a715c7b44f90
Lajoui Zakariae <lajoui.zakariae.1@gmail.com>
2023-08-20 05:45:50 +0200

    working on product form

--- resources/js/Products/Add.jsx
+++ resources/js/Products/Add.jsx
@@ -21,7 +21,12 @@ export default function AddProduct() {
         axios.get("/api/categories").then(({ data }) => data)
     );
 
-    const [tab, setTab] = useState(1);
+    const { data: colors, isLoading: isColorsLoading } = useQuery(
+        "colors",
+        () => axios.get("/api/colors").then(({ data }) => data)
+    );
+
+    const [tab, setTab] = useState(2);
 
     const [product, setProduct] = useState({
         title: "",
@@ -33,6 +38,8 @@ export default function AddProduct() {
     });
 
     const fileInput = useRef();
+
+    console.log(product.images);
 
     return (
         <>
@@ -135,42 +142,89 @@ export default function AddProduct() {
                 ) : null}
                 {tab === 2 ? (
                     <div>
-                        <input
-                            type="file"
-                            name="images"
-                            ref={fileInput}
-                            hidden
-                            onChange={(ev) => {
-                                const fileReader = new FileReader();
-
-                                fileReader.readAsDataURL(ev.target.files[0]);
-
-                                fileReader.onload = (data) => {
-                                    const imgAsUrl = data.currentTarget.result;
-                                    setProduct((prev) => ({
-                                        ...prev,
-                                        images: [...prev.images, imgAsUrl],
-                                    }));
-                                };
-                            }}
-                        />
                         <div className="flex flex-wrap">
                             <div className="basis-1/3 px-2 py-2">
-                                <div
-                                    className="flex justify-center items-center h-36 bg-gray-200 hover:bg-gray-100 cursor-pointer transition duration-500 rounded-md"
-                                    onClick={() => fileInput.current.click()}
-                                >
-                                    Add
-                                </div>
+                                <>
+                                    <input
+                                        type="file"
+                                        name="images"
+                                        ref={fileInput}
+                                        hidden
+                                        onChange={(ev) => {
+                                            const fileReader = new FileReader();
+
+                                            fileReader.readAsDataURL(
+                                                ev.target.files[0]
+                                            );
+
+                                            fileReader.onload = (data) => {
+                                                const imgAsUrl =
+                                                    data.currentTarget.result;
+                                                setProduct((prev) => ({
+                                                    ...prev,
+                                                    images: [
+                                                        ...prev.images,
+                                                        {
+                                                            url: imgAsUrl,
+                                                        },
+                                                    ],
+                                                }));
+                                            };
+                                        }}
+                                    />
+                                    {
+                                        <div>
+                                            <>
+                                                {isColorsLoading ? (
+                                                    <p>Loading...</p>
+                                                ) : (
+                                                    <select
+                                                        className="w-full rounded-t-md"
+                                                        onChange={(ev) =>
+                                                            setColor(
+                                                                ev.target.value
+                                                            )
+                                                        }
+                                                    >
+                                                        {colors.map(
+                                                            ({
+                                                                id,
+                                                                name,
+                                                                hex,
+                                                            }) => (
+                                                                <option
+                                                                    key={id}
+                                                                    value={id}
+                                                                    className="w-full h-5"
+                                                                    style={{
+                                                                        backgroundColor:
+                                                                            hex,
+                                                                    }}
+                                                                >
+                                                                    {name}
+                                                                </option>
+                                                            )
+                                                        )}
+                                                    </select>
+                                                )}
+                                                <button
+                                                    className="bg-slate-950 hover:bg-slate-800 transition duration-700 text-white py-1 px-3 rounded-md"
+                                                    onClick={() =>
+                                                        fileInput.current.click()
+                                                    }
+                                                >
+                                                    Add Image
+                                                </button>
+                                            </>
+                                        </div>
+                                    }
+                                </>
                             </div>
-                            {product.images.map((image) => (
-                                <div
-                                    key={image}
-                                    className="basis-1/3 px-2 py-2"
-                                >
+                            {product.images.map(({ url }) => (
+                                <div key={url} className="basis-1/3 px-2 py-2">
                                     <div className="flex justify-center items-center h-36 bg-gray-200 hover:bg-gray-100 transition">
                                         <img
-                                            src={image}
+                                            src={url}
                                             className="h-full w-full object-cover rounded-md"
                                         />
                                     </div>
